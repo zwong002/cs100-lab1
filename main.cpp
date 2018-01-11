e175cc8840c50d71e344a3fd88a156719bb7f8a9
Zachary Wong <zwong002@hammer.cs.ucr.edu>
2018-01-11 15:35:23 -0800

    added user input

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 int main()
 {
 
-	std::cout << "hello git" << std::endl;
+	std::string name;
+	std::cout << "What is your name?" << std::endl;
+	std::cin >> name;
+	std::cout << "Helli " << name << "!" << std:: endl;
+
 	return 1;
 }
 
